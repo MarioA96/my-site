8ee4a1908ffa9d8f98d092726ec56bb1af009e3b
MarioA96 <marioaquirozs@gmail.com>
2025-04-01 19:33:50 -0600

    🔧Add color palette module for consistent styling across components

---
+++ src/styles/color-palette.ts
@@ -0,0 +1,6 @@
+export const colorPalette = {
+    primaryHeadingFontColor: 'rgb(163,118,254)',
+    primaryFontColor: 'white',
+    primaryCardBgColor: 'rgb(31,31,31)',
+    primaryMuteFontColor: 'rgb(152,152,152)'
+}
